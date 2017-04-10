c6dfb2afd93e8add9bc90f16a68e7914797a61df
Louis Tran <loutran987@gmail.com>
2017-04-10 15:43:25 -0700

    Add Sinon stubs to order-spec.js

--- testing-and-debugging/test/order-spec.js
+++ testing-and-debugging/test/order-spec.js
@@ -20,8 +20,13 @@ describe('Ordering Items', function() {
       log: sinon.spy()
     };
 
+    this.warehouse = {
+      packageAndShip: sinon.stub().yields(10987654321)
+    };
+
     order.__set__('inventoryData', this.testData);
     order.__set__('console', this.console);
+    order.__set__('warehouse', this.warehouse);
 
   });
 
@@ -32,6 +37,25 @@ describe('Ordering Items', function() {
     order.orderItem('CCC', 3, function() {
       expect(_this.console.log.callCount).to.equal(2);
       done();
+    });
+
+  });
+
+  describe('Warehouse interaction', function () {
+
+    beforeEach(function() {
+
+      this.callback = sinon.spy();
+      order.orderItem('CCC', 2, this.callback);
+
+    });
+
+    it('receives a tracking number', function() {
+      expect(this.callback.calledWith(10987654321)).to.equal(true);
+    });
+
+    it('calls packageAndShip with the correct sku and quantity', function() {
+      expect(this.warehouse.packageAndShip.calledWith('CCC', 2)).to.equal(true);
     });
 
   });
